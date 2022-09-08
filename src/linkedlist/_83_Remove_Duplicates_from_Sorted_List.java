164811e998eb68294de788bc7b64904e0c060fa5
Richard Phoenix <r1ch4rdph0en1x@gmail.com>
2022-09-08 07:02:53 -0700

    Problem 83

---
+++ src/linkedlist/_83_Remove_Duplicates_from_Sorted_List.java
@@ -0,0 +1,19 @@
+package linkedlist;
+
+/**
+ * @author Richard Phoenix
+ */
+public class _83_Remove_Duplicates_from_Sorted_List {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode node = head;
+        while (node != null && node.next != null) {
+            if (node.val == node.next.val) {
+                node.next = node.next.next;
+            } else {
+                node = node.next;
+            }
+        }
+
+        return head;
+    }
+}
