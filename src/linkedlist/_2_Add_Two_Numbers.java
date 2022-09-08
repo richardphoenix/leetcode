49d5c1c863eff5ead090ef8f1df39383ee36215f
Richard Phoenix <r1ch4rdph0en1x@gmail.com>
2022-09-08 10:39:13 -0700

    Problem 2

---
+++ src/linkedlist/_2_Add_Two_Numbers.java
@@ -0,0 +1,25 @@
+package linkedlist;
+
+/**
+ * @author Richard Phoenix
+ */
+public class _2_Add_Two_Numbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode node = new ListNode();
+        ListNode curr = node;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+            int sum = x + y + carry;
+
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        return node.next;
+    }
+}
