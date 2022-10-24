082ee57fa36b8395a12ebd3ffa4d6310553f85e2
Richard <r1ch4rdph0en1x@gmail.com>
2022-10-24 04:30:03 -0700

    438. Find All Anagrams in a String

---
+++ src/string/_438_Find_All_Anagrams_in_a_String.java
@@ -0,0 +1,40 @@
+package string;
+
+import java.util.*;
+
+/**
+ * https://leetcode.com/problems/find-all-anagrams-in-a-string/
+ * @author Richard Phoenix
+ */
+public class _438_Find_All_Anagrams_in_a_String {
+    public List<Integer> findAnagrams(String s, String p) {
+        int ns = s.length(), np = p.length();
+        if (ns < np) return new ArrayList();
+
+        int [] pCount = new int[26];
+        int [] sCount = new int[26];
+        // build reference array using string p
+        for (char ch : p.toCharArray()) {
+            pCount[(int)(ch - 'a')]++;
+        }
+
+        List<Integer> output = new ArrayList();
+        // sliding window on the string s
+        for (int i = 0; i < ns; i++) {
+            // add one more letter
+            // on the right side of the window
+            sCount[(int)(s.charAt(i) - 'a')]++;
+            // remove one letter
+            // from the left side of the window
+            if (i >= np) {
+                sCount[(int)(s.charAt(i - np) - 'a')]--;
+            }
+            // compare array in the sliding window
+            // with the reference array
+            if (Arrays.equals(pCount, sCount)) {
+                output.add(i - np + 1);
+            }
+        }
+        return output;
+    }
+}
